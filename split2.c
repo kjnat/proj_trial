eaf96663fee42de152321c6318f5b7c4fc4c0dfb
kjnat <46638938+kjnat@users.noreply.github.com>
2021-08-16 21:48:48 +0000

    Update split2.c

--- split2.c
+++ split2.c
@@ -12,7 +12,7 @@ int main()
     fgets(str1, sizeof str1, stdin);	
  
     j=0; ctr=0;
-    for(i=0; i<=(strlen(str1)); i++)
+    for(i=0; i <= (strlen(str1)); i++)
     {
         // if space or NULL found, assign NULL into newString[ctr]
         if(str1[i]==' '||str1[i]=='\0')
