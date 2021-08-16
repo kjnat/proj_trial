4a17020aa999abd8be02a780e6cda44fbe246a68
kjnat <46638938+kjnat@users.noreply.github.com>
2021-08-16 17:54:46 +0000

    Create av.c

---
+++ av.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Function that prints all the arguments, without using ac.
+ * @ac: ac is the number of items in av
+ * @av: av is a NULL terminated array of strings
+ * Return: 0
+ */
+ int main(int ac, char **av)
+{
+	int i;
+
+	for (i = 0; i < ac; i++)
+		printf("%s\n", av);
+	return (0);
+}
